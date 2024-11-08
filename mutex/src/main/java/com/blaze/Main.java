26a282d39af3f7dea19c9a11e6d0ab1bb0e3d10c
blaze <plusstep_@naver.com>
2024-11-08 16:26:56 +0900

    feat : 단일 프로젝트 멀티 모듈 구조 적용
    Mutex 모듈 추가

---
+++ mutex/src/main/java/com/blaze/Main.java
@@ -0,0 +1,18 @@
+package com.blaze;
+
+/**
+ * packageName    : com.blaze
+ * fileName       : ${NAME}
+ * author         : Min
+ * date           : 2024-11-08
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-11-08        Min       최초 생성
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
