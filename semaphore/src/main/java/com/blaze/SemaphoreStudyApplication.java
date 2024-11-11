9b033219f43a6318048db8bcb8c8eec490cb7606
blaze <plusstep_@naver.com>
2024-11-11 15:58:30 +0900

    feat : 세마포어스터디 빌드 및 실행 설정

---
+++ semaphore/src/main/java/com/blaze/SemaphoreStudyApplication.java
@@ -0,0 +1,42 @@
+package com.blaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class SemaphoreStudyApplication {
+    public static final String BASE_YML = "classpath:";
+    public static final String CONFIG_LOCATION = "spring.config.location=";
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        String yml = getYML();
+
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(SemaphoreStudyApplication.class);
+        springApplicationBuilder.properties(yml);
+
+        SpringApplication springApplication = springApplicationBuilder.build();
+        springApplication.run(args);
+    }
+
+    public static String getYML() {
+        String applicationYML = getApplicationYML();
+        String datasourceYML = getDatasourceYML();
+
+        return CONFIG_LOCATION +
+                applicationYML +
+                datasourceYML;
+    }
+
+    public static String getApplicationYML() {
+        return BASE_YML + "/application.yml;";
+    }
+
+    public static String getDatasourceYML() {
+        return BASE_YML + "/datasource.yml";
+    }
+}
